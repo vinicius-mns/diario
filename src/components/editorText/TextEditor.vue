9b06deea20c83bb509c84b8fd501ed541246cfaa
vinicius silva <vinicius_dev@hotmail.com>
2023-07-19 00:39:39 -0300

    refactor(TextEditor.vue): Refactorar todo component com comentarios
    refatora o component de edicao de texto markdown, com comentários no css. e adicionando o store toggleComponents para visulizar o component PreviewText.vue quando o TextEditor é focado
    modified:   src/components/editorText/TextEditor.vue

--- src/components/editorText/TextEditor.vue
+++ src/components/editorText/TextEditor.vue
@@ -1,53 +1,64 @@
 <script setup lang="ts">
 import { useMarkdownStore } from '../../stores/markdown';
+import { useToggleComponents } from '../../stores/toggleComponents';
 
 const markdownStore = useMarkdownStore()
+const toggleComponents = useToggleComponents()
 
 const edit = (event: Event) => {
   const { value } = event.target as HTMLTextAreaElement
   markdownStore.editValue(value)
 }
+
+const placeholder = `# Dia de hoje
+  ---
+  ## Testando
+  - item um
+  - item 2
+`
 </script>
 
 <template>
   <main class="text-editor-container">
-    <div class="textarea-container">
-      <textarea @input="edit"></textarea>
-    </div>
+    <textarea @input="edit" :placeholder="placeholder"
+    @focus="toggleComponents.togglePreviewText" @blur="toggleComponents.togglePreviewText"
+    ></textarea>
   </main>
 </template>
 
 <style scoped lang="scss">
 @media screen and (max-width: 700px) {
   .text-editor-container {
-    position: absolute;
-    bottom: 0;
-    margin-bottom: 1.5rem;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 20vh;
-    
-    .textarea-container {
-      height: 100%;
-      width: 90%;
-      display: flex;
-      align-items: center;
-      justify-content: center;
+    textarea {
+      // posicionamento
+      position: fixed;
+      bottom: 0;
+      margin-bottom: 1rem;
 
-      textarea {
-        border-radius: 1rem;
-        background-color: rgb(129, 188, 122);
-        width: 100%;
-        height: 100%;
-        padding: 1rem;
-        font-size: 1rem;
-        resize: none;
-        border: none;
-        outline: none;
-      }
+      // estilo
+      border-radius: 2rem;
+      background-color: rgb(129, 188, 122);
+
+      // tamanho
+      width: 96%;
+      height: 6vh;
+      left: 2%;
+      font-size: 1rem;
+      box-sizing: border-box;
+      padding: 1rem;
+
+      // transicao quando :focus acontecer
+      transition: 0.5s all;
+
+      // remove valores padros do textarea
+      resize: none;
+      border: none;
+      outline: none;
+    }
+
+    textarea:focus {
+      border-radius: 1rem;
+      height: 20vh;
     }
   }
 }
