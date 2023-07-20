2fa9face8dd01d63d44905dd9d1bf0cdb1a6f48d
vinicius silva <vinicius_dev@hotmail.com>
2023-07-20 14:05:18 -0300

    feat(TopBar.vue): Criar component topBar
    Cria component responsavel por
    - conter o botao de configuracao
    - manter o logo
    new file:   src/components/topBar/TopBar.vue

---
+++ src/components/topBar/TopBar.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header class="top-bar">
+    <div class="logo"></div>
+    <button class="config">⚙️</button>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+  .top-bar {
+  // posicionamento
+  position: fixed;
+  top: 0;
+
+  // display
+  display: flex;
+
+  // tamanho
+  height: 6vh;
+  width: 100%;
+  
+  // estilo
+  background-color: pink;
+
+  .config {
+    height: 100%;
+    aspect-ratio: 1;
+    justify-self: end;
+  }
+
+  .logo {
+    height: 100%;
+    aspect-ratio: 4/1;
+
+    background-color: red;
+  }
+}
+}
+</style>
