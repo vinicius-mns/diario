3a9e3f0795ade6e17928a026ebcc2285e92c179e
vinicius silva <vinicius_dev@hotmail.com>
2023-07-20 13:01:19 -0300

    feat(DayCard.vue): Criar component DayCard
    Cria compoment para:
    - renderizar o conteudo do dia escrito
    - marcar a data de criacao do dia
    - (futuramente) permitir a edicao de um dia (ou nao, to pensando ainda se vou permitir)
    new file:   src/components/dayCard/DayCard.vue

---
+++ src/components/dayCard/DayCard.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { marked } from 'marked';
+
+marked.setOptions({
+  breaks: true,
+})
+
+const props = defineProps<{content: string}>()
+
+const markedV = marked(props.content, { mangle: false, headerIds: false });
+</script>
+
+<template>
+  <div class="day-card">
+    <div class="date"><p>19/07/23</p></div>
+    <div
+      class="content"
+      v-html="markedV"
+    ></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+  .day-card {
+    width: 98%;
+    max-height: 33vh;
+    background-color: white;
+    border-radius: 1rem;
+    margin-top: 5px;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    // impedir o encolhimento por estar dentro de um container display flex
+    flex-shrink: 0;
+
+    .content {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+</style>
