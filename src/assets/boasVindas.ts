b2e5cdd42afbefad71651bcf311816084cd0eb7c
vinicius silva <vinicius_dev@hotmail.com>
2023-07-22 10:51:00 -0300

    feat(boasVindas.ts): Criar arquivo base de boas boasVindas
    Cria arquivo que conterá a mensagem de boas vindas ao usuario. A menssagem vai guiar o usuaio e servira de exemplo de renderização de carda
    	new file:   src/assets/boasVindas.ts

---
+++ src/assets/boasVindas.ts
@@ -0,0 +1,8 @@
+export const boasVindas = `# Boas vindas ao diário
+---
+## Como funciona ?
+  - simples
+  - facil
+  - intuitivo
+  **esse será o primeiro card que servira não so como exemplo mas tambem ensinará como o usário como o app funciona**
+`
