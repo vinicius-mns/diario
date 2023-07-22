94c84afe2cf83d9e973f918b05be9d4de9ff0dfb
vinicius silva <vinicius_dev@hotmail.com>
2023-07-22 11:03:30 -0300

    feat(diary.ts): Iniciar valores de estado global para diary
    Inicia valores de estado global para diario usando:
    - classes handle:
      - localStorage: Para adcionar dados caso nao exista / capturar dados / atualizar dados
    - initState:
      - iniciar novo card na primeira visita
    - declarar estado inicial usando classe helper
    	modified:   src/stores/diary.ts

--- src/stores/diary.ts
+++ src/stores/diary.ts
@@ -1,6 +1,15 @@
+import { HandleLocalStorage } from '@/utils'
+import { type IDay } from '@/interfaces'
 import { defineStore } from 'pinia'
-
+import { reactive } from 'vue'
+import { boasVindas } from '@/assets/boasVindas'
 
 const diaryStore = 'diaryStore'
 export const useDiaryStore = defineStore(diaryStore, () => {
+
+  const initState: IDay[] = [{ date: new Date(), content: boasVindas }]
+
+  const localStorageDiary = new HandleLocalStorage(diaryStore, initState)
+  
+  const state = reactive(localStorageDiary.read)
 })
