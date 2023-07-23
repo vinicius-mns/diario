b16593383997e88a23e1e6db1dff627ad4676818
vinicius silva <vinicius_dev@hotmail.com>
2023-07-23 18:43:34 -0300

    feat(router): Adicionar rota /diary
    Rota que ira renderizar o component DiaryPage.vue
    	modified:   src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import DiaryPage from '@/views/DiaryPage.vue'
 import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      redirect: 'diary'
+    },
+    {
+      path: '/diary',
+      name: 'diary',
+      component: DiaryPage
+    },
     {
       path: '/notFound',
       name: 'not found',
