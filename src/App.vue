f9c14feaceb83eb58fc85efcdf0f544c3502de07
vinicius silva <vinicius_dev@hotmail.com>
2023-07-23 18:26:26 -0300

    refactor(App.vue): Usar rotas no App
    Usar routerView no app para renderizaçao das rotas
    	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <script setup lang="ts">
-import textEditor from './components/editorText/TextEditor.vue'
-import previewText from './components/previewText/PreviewText.vue';
-import DiaryPageVue from './views/DiaryPage.vue';
-import TopBar from './components/topBar/TopBar.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div class="app">
-    <div class="container">
-      <previewText />
-      <textEditor />
-      <DiaryPageVue />
-      <TopBar />
-    </div>
+    <RouterView />
   </div>
 </template>
 
 <style scoped lang="scss">
 @media screen and (max-width: 700px) {
-  .container {
-    display: flex;
-    flex-direction: column;
-  }
 }
 </style>
