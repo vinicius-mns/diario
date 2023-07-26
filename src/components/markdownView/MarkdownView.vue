1480a52e8e95d1ea48dd059da8f9ba8d2308eed4
vinicius silva <vinicius_dev@hotmail.com>
2023-07-26 16:32:13 -0300

    feat(markdownView.vue): Criar component para renderizar texto markdownView
    	new file:   src/components/markdownView/MarkdownView.vue

---
+++ src/components/markdownView/MarkdownView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { marked } from 'marked'
+import { computed } from 'vue';
+
+marked.setOptions({ breaks: true })
+
+const options = { mangle: false, headerIds: false }
+
+const props = defineProps<{ content: string }>()
+
+const value = computed(() => marked(props.content, options))
+</script>
+
+<template>
+  <div
+    class="content"
+    v-html="marked(value)"
+  ></div>
+</template>
+
+<style lang="scss">
+@media screen and (max-width: 700px) {
+  .content {
+    width: 90%;
+    text-align: justify;
+
+    transition: all 0.5s
+  }
+}
+</style>
