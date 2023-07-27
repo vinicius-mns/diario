12643107d6df6ce4afa6bb73fdd5d04f2acdafd9
vinicius silva <vinicius_dev@hotmail.com>
2023-07-27 12:26:10 -0300

    feat(DiaryPageId.vue): Adiciona estilo ao botao de voltar 	modified:   src/views/DiaryPageId.vue

--- src/views/DiaryPageId.vue
+++ src/views/DiaryPageId.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MarkdownView from '@/components/markdownView/MarkdownView.vue';
 import { useDiaryStore } from '@/stores/diary';
+import { useStyle } from '@/stores/style';
 import { HandleDate } from '@/utils';
 import { onMounted, reactive } from 'vue'
 import { useRoute, useRouter } from 'vue-router';
@@ -10,6 +11,7 @@ const state = reactive({ content: '', date: '' })
 const diary = useDiaryStore()
 const route = useRoute()
 const router = useRouter()
+const style = useStyle()
 
 onMounted(() => {
   const { id } = route.params
@@ -28,7 +30,7 @@ onMounted(() => {
   <div class="render-day">
     <div>{{ state.date }}</div>
     <MarkdownView :content="state.content" />
-    <RouterLink to="/diary"><button>Voltar</button></RouterLink>
+    <RouterLink to="/diary"><button class="back">Voltar</button></RouterLink>
   </div>
 </template>
 
@@ -39,6 +41,28 @@ onMounted(() => {
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    & .back {
+      // posicao
+      position: fixed;
+      
+      // medidas
+      width: 50%;
+      left: 25%;
+      bottom: 30px;
+      height: 40px;
+
+      // estilo
+      background-color: v-bind('style.value.baseColor');
+      border-radius: v-bind('style.value.borderRadius');
+      box-shadow: v-bind('style.value.boxShadow');
+
+      overflow: hidden;
+
+      // button estilo
+      border: none;
+      cursor: pointer;
+    }
   }
 }
 </style>
