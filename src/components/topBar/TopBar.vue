2a8968d5ba35b74fbaf9438e6447c11a53ad1ac7
vinicius silva <vinicius_dev@hotmail.com>
2023-07-27 10:12:13 -0300

    feat(topBar.vue): Definir estilo usando estado global 	modified:   src/components/topBar/TopBar.vue

--- src/components/topBar/TopBar.vue
+++ src/components/topBar/TopBar.vue
@@ -1,42 +1,52 @@
 <script setup lang="ts">
+import { useStyle } from '@/stores/style';
+
+const style = useStyle()
 </script>
 
 <template>
   <header class="top-bar">
-    <div class="logo"></div>
-    <button class="config">⚙️</button>
+    <button class="logo">D I A R I O</button>
   </header>
 </template>
 
 <style scoped lang="scss">
 @media screen and (max-width: 700px) {
   .top-bar {
-  // posicionamento
-  position: fixed;
-  top: 0;
+    // posicionamento
+    position: fixed;
+    top: 0;
 
-  // display
-  display: flex;
+    // display
+    display: flex;
+    align-items: center;
 
-  // tamanho
-  height: 6vh;
-  width: 100%;
-  
-  // estilo
-  background-color: pink;
+    // tamanho
+    height: 6vh;
+    width: 100%;
+    
+    // estilo
+    background-color: v-bind('style.value.pageColor');
+    box-shadow: v-bind('style.value.boxShadow');
 
-  .config {
-    height: 100%;
-    aspect-ratio: 1;
-    justify-self: end;
+    .logo {
+      // medidas
+      height: 100%;
+      aspect-ratio: 4/1;
+      
+      // display
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      // estilo
+      color: v-bind('style.value.especialColor');
+      
+      // button estilo  
+      border: none;
+      background-color: transparent;
+      cursor: pointer;
+    }
   }
-
-  .logo {
-    height: 100%;
-    aspect-ratio: 4/1;
-
-    background-color: red;
-  }
-}
 }
 </style>
