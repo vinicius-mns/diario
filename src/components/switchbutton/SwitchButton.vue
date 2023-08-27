6660f1117cc7f94409d0cd4749eba85f6bb966db
vinicius silva <vinicius_dev@hotmail.com>
2023-08-27 19:08:11 -0300

    feat(SwitchButton.vue): Adicionar novo component switchbutton
    Component responsavel por configurar alteracoes booleanas
    
    contem as seguintes propriedades
      props:
        usar o define props para receber como diretivas:
          height -> para definir o tamanho do botao
          title -> para definir um title, um descritivo do que vai ser alterado
          toggleButton -> funcao sem retorno que sera chamada sempre que o click for ativado
          state -> para definir se esta ativo ou nao, recebendo um booleano
    
    exemplo de uso:
      <SwitchButton title="modo escuro" :state="trueOrFalse" :toggleButton="handleFunction" />
    
    	new file:   src/components/switchbutton/SwitchButton.vue

---
+++ src/components/switchbutton/SwitchButton.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+const props = defineProps({
+  height: {
+    type: String,
+    default: '28px',
+  },
+  title: {
+    type: String,
+    default: ''
+  },
+  state: {
+    type: Boolean,
+    default: false,
+  },
+  togglebutton: {
+    type: Function,
+    default: (): void => {},
+  },
+})
+</script>
+
+<template>
+  <div class="switch-container">
+    <p>{{ title }}</p>
+    <button class="toggle-button" @click="togglebutton()" :class="JSON.stringify(state)">
+      <div class="indicator" :class="JSON.stringify(state)">
+        <div class="circle"></div>
+      </div>
+    </button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+  .switch-container {
+    // medidas
+    width: 100%;
+
+    // display
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    & p {
+      margin: 0;
+      padding: 0;
+      font-size: 18px;
+    }
+
+    & .toggle-button {
+      // medidas
+      height: v-bind('props.height');
+      aspect-ratio: 2;
+      margin: 0;
+      padding: 0;
+  
+      // estilo
+      background-color: rgba(0, 0, 0, 0.5);
+      border: none;
+      border-radius: 50px;
+      cursor: pointer;
+  
+      // display
+      display: flex;
+      align-items: center;
+  
+      & .indicator {
+        // medidas
+        height: 100%;
+        aspect-ratio: 1;
+        margin: 0;
+        padding: 0;
+      
+        // display
+        display: flex;
+        align-items: center;
+        justify-content: center;
+  
+        // estilo
+        background-color: transparent;
+        
+        // animation
+        transition: 0.2s all;
+      
+        & .circle {
+          // medidas
+          height: 58%;
+          aspect-ratio: 1;
+  
+          // estilo
+          border-radius: 50%;
+          font-size: calc(v-bind('props.height') * 1);
+          background-color: white;
+        }
+      }
+  
+      & .true {
+        margin-left: 50%;
+      }
+    }
+
+    & .true {
+      background-color: rgb(113, 171, 113);
+    }
+  }
+}
+</style>
