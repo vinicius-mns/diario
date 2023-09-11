d8203ce3c2ae288aab5a28202a7cf21175663d9b
vinicius silva <vinicius_dev@hotmail.com>
2023-09-11 17:40:53 -0300

    feat(RegisterPage): Adicionar css estilo a pagina de registro 	modified:   src/views/RegisterPage.vue

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+const style = useStyle()
 </script>
 
 <template>
   <main class="register-container">
-    <div>
-      <h1>Cria sua conta:</h1>
-    </div>
-    <form @submit.prevent="onSubmit">
-      <div>
+    <form @submit.prevent="onSubmit" class="form">
+      <div class="title">
+        <h1>Crie sua conta</h1>
+      </div>
+
+      <div class="mini-form">
         <label for="username"></label>
         <input
           type="text"
@@ -16,7 +18,7 @@
           placeholder="Email"
         />
       </div>
-      <div>
+      <div class="mini-form">
         <label for="password"></label>
         <input
           type="password"
@@ -25,7 +27,7 @@
           placeholder="Senha"
         />
       </div>
-      <div>
+      <div class="mini-form">
         <label for="password"></label>
         <input
           type="password"
@@ -34,7 +36,110 @@
           placeholder="Repetir senha"
         />
       </div>
-      <button type="submit">Criar conta</button>
+      <button type="submit"><p>Criar contar</p></button>
     </form>
   </main>
 </template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+  .register-container {
+    // estilo
+    color: v-bind('style.value.textColor');
+
+    // medidas
+    height: 100vh;
+    width: 100%;
+
+    // display
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & .form {
+      // display
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      // estilo
+      background-color: v-bind('style.value.baseColor');
+      box-shadow: v-bind('style.value.boxShadow');
+      border-radius: v-bind('style.value.borderRadius');
+      overflow: hidden;
+
+      // medidas
+      width: 90%;
+
+      & .title {
+        // estilo
+        background-color: v-bind('style.value.especialColor');
+        background-color: v-bind('style.value.baseColor');
+
+        // medidas
+        height: 50px;
+        width: 100%;
+
+        // display
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        & h1 {
+          margin: 0;
+          padding: 0;
+          font-size: large;
+        }
+      }
+
+      & .mini-form {
+        // display
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        // medidas
+        width: 100%;
+        margin-top: 20px;
+        
+        & input {
+          // medidas
+          width: 90%;
+          height: 42px;
+          font-size: 16px;
+
+          // estilo
+          background-color: v-bind('style.value.pageColor');
+          color: v-bind('style.value.textColor');
+          border-radius: 4px;
+          text-indent: 30px;
+          outline: none;
+          border: none;
+        }
+      }
+
+      & button {
+        // medidas
+        width: 90%;
+        height: 38px;
+
+        // posicionamento
+        margin-top: 20px;
+        margin-bottom: 20px;
+        
+        // estilo
+        color: v-bind('style.value.textColor');
+        background-color: v-bind('style.value.especialColor');
+        border-radius: 4px;
+        border: none;
+
+        & p {
+          margin: 0;
+          padding: 0;
+          font-size: 18px;
+        }
+      }
+    }
+  }
+}
+</style>
