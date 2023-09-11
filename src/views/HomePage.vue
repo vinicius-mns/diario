ba5721b91d37ef7e43498033c9d48a7196063901
vinicius silva <vinicius_dev@hotmail.com>
2023-09-11 15:54:21 -0300

    feat(HomePage): Adiciona rotas para nevegacao no site [diary] [register] [login] 	modified:   src/views/HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,7 +3,11 @@
 
 <template>
   <main class="home-page">
-
+    <nav>
+      <RouterLink to="/diary"><button><h2>Entrar sem conta</h2></button></RouterLink>
+      <RouterLink to="/register"><button><h2>Crie uma conta</h2></button></RouterLink>
+      <RouterLink to="/login"><button><h2>Logar</h2></button></RouterLink>
+    </nav>
   </main>
 </template>
 
