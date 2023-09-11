b2485777f0f3177bef3e95a630c8016cf9b71716
vinicius silva <vinicius_dev@hotmail.com>
2023-09-11 18:51:16 -0300

    feat(LoginPage.vue): Criar component semelhante ao register.vue
    contendo inputs:
    [email]
    [senha]
    [lembrar de mim]
    [botao de login]
    [redirecionar para registro]
    
    Junto com css semelhante a pagina de registro de usuario
    	new file:   src/views/LoginPage.vue

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,169 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import { useStyle } from '@/stores/style';
+import SwitchButton from '@/components/switchbutton/SwitchButton.vue';
+
+const style = useStyle()
+
+const remember = ref(false)
+
+const toggleRemember = () => {
+  remember.value = !remember.value
+  console.log(remember.value)
+}
+
+</script>
+
+<template>
+  <main class="login-container">
+    <form @submit.prevent="onSubmit" class="form">
+      <div class="title">
+        <h1>Entre na sua conta</h1>
+      </div>
+
+      <div class="mini-form">
+        <label for="username"></label>
+        <input
+          type="text"
+          id="email"
+          v-model="content.email"
+          placeholder="Email"
+        />
+      </div>
+      <div class="mini-form">
+        <label for="password"></label>
+        <input
+          type="password"
+          id="password"
+          v-model="content.password"
+          placeholder="Senha"
+        />
+      </div>
+      <div class="mini-form">
+        <SwitchButton title="Lembrar de mim" :togglebutton="toggleRemember" :state="remember"/>
+      </div>
+      <button type="submit"><p>Entrar</p></button>
+      <RouterLink to="/register">Cria uma conta</RouterLink>
+    </form>
+  </main>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+  .login-container {
+    // estilo
+    color: v-bind('style.value.textColor');
+
+    // medidas
+    height: 100vh;
+    width: 100%;
+
+    // display
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & .form {
+      // display
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      // estilo
+      background-color: v-bind('style.value.baseColor');
+      box-shadow: v-bind('style.value.boxShadow');
+      border-radius: v-bind('style.value.borderRadius');
+      overflow: hidden;
+
+      // medidas
+      width: 90%;
+
+      & .title {
+        // estilo
+        background-color: v-bind('style.value.especialColor');
+        background-color: v-bind('style.value.baseColor');
+
+        // medidas
+        height: 50px;
+        width: 100%;
+
+        // display
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        & h1 {
+          margin: 0;
+          padding: 0;
+          font-size: large;
+        }
+      }
+
+      & .mini-form {
+        // display
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        // medidas
+        width: 90%;
+        margin-top: 20px;
+        
+        & input {
+          // medidas
+          width: 100%;
+          height: 42px;
+          font-size: 16px;
+
+          // estilo
+          background-color: v-bind('style.value.pageColor');
+          color: v-bind('style.value.textColor');
+          border-radius: 4px;
+          text-indent: 30px;
+          outline: none;
+          border: none;
+        }
+
+        & input::placeholder {
+          color: v-bind('style.value.textColor');
+          opacity: 55%;
+        }
+      }
+
+      & button {
+        // medidas
+        width: 90%;
+        height: 38px;
+
+        // posicionamento
+        margin-top: 20px;
+        margin-bottom: 20px;
+        
+        // estilo
+        color: white;
+        background-color: rgb(82, 82, 199);
+        border-radius: 4px;
+        border: none;
+        box-shadow: black 4px 4px;
+        cursor: pointer;
+
+        & p {
+          margin: 0;
+          padding: 0;
+          font-size: 18px;
+        }
+      }
+
+      & button:active {
+        box-shadow: 0px 0px;
+        transform: translate(4px, 4px);
+      }
+    }
+
+    & a {
+      color: v-bind('style.value.textColor');
+      margin-bottom: 5px;
+    }
+  }
+}
+</style>
