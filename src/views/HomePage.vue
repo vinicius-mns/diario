b4baff5769f4b1862cb26de36ad347d9b91bf102
vinicius silva <vinicius_dev@hotmail.com>
2023-09-11 15:35:53 -0300

    feat(HomePage.vue): Criar component vazio 	new file:   src/views/HomePage.vue

---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="home-page">
+
+  </main>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+  .home-page {
+    
+  }
+}
+</style>
