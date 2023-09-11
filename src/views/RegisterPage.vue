6328996b33cb71c36c65a6f02da6ef1f26b5c2a6
vinicius silva <vinicius_dev@hotmail.com>
2023-09-11 16:46:06 -0300

    feat(RegisterPage.vue): Criar pagina para criacao de conta 	new file:   src/views/RegisterPage.vue

---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="register-container">
+    <div>
+      <h1>Cria sua conta:</h1>
+    </div>
+    <form @submit.prevent="onSubmit">
+      <div>
+        <label for="username"></label>
+        <input
+          type="text"
+          id="email"
+          v-model="content.email"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <label for="password"></label>
+        <input
+          type="password"
+          id="password"
+          v-model="content.password"
+          placeholder="Senha"
+        />
+      </div>
+      <div>
+        <label for="password"></label>
+        <input
+          type="password"
+          id="password2"
+          v-model="content.password2"
+          placeholder="Repetir senha"
+        />
+      </div>
+      <button type="submit">Criar conta</button>
+    </form>
+  </main>
+</template>
