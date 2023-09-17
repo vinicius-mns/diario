8718f8ffd4b51ef8232308ab80052acf5f14405c
vinicius silva <vinicius_dev@hotmail.com>
2023-09-17 19:55:49 -0300

    Refactor(LoadingComponent): Adicionar estado glogal e definir css
    - Usar estado glocal toggleComponents para definir a visibilidade do loading
    - Adicionar estilo
      - back-loading-container -> cobre 100% de toda area da tela
        - loading-card -> Card que envolve todo o loading
          - loading-style -> Contem o formato e a animacao do loagin
    	modified:   src/components/loading/LoadingComponent.vue

--- src/components/loading/LoadingComponent.vue
+++ src/components/loading/LoadingComponent.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 import { useStyle } from '@/stores/style';
+import { useToggleComponents } from '@/stores/toggleComponents';
+
+const toggleComponent = useToggleComponents()
 
 const style = useStyle()
 </script>
 
 <template>
-  <div class="back-loading-container">
-    <div class="loading-container">
-      <div class="polygon-container">
-        <div class="polygon"></div>
+  <div class="back-loading-container" v-if="toggleComponent.globalLoading">
+    <div class="loading-card">
+      <div class="loading-style">
+        <div class="polygon-container">
+          <div class="polygon"></div>
+        </div>
+        <div class="ring"></div>
       </div>
-      <div class="transparent-circle"></div>
     </div>
   </div>
 </template>
@@ -34,68 +39,68 @@ const style = useStyle()
     align-items: center;
     justify-content: center;
 
-    .loading-container {
+    & .loading-card {
       // medidas
       height: 80px;
       aspect-ratio: 1;
   
       // estilo
       background-color: v-bind('style.value.baseColor');
-      box-shadow: v-bind('style.value.boxShadow');
       border-radius: 10px;
   
       // posicionamento
       position: relative;
-  
-      // animacao
-      animation: spin 2s infinite;
-      
-      & .transparent-circle {
-        // medidas
-        height: 60%;
-        width: 60%;
-  
-        // posicionamento
-        position: absolute;
-        top: 20%;
-        left: 20%;
-  
-        // estilo
-        border-radius: 50%;
-        background-color: v-bind('style.value.baseColor');
-        border: v-bind('style.value.especialColor') solid 4px;
-        box-sizing: border-box;
-      }
-  
-      .polygon-container {
-        // medidas
-        height: 80%;
-        width: 80%;
-  
-        // estilo
-        border-radius: 50%;
-  
-        // posicionamento
-        position: absolute;
-        top: 10%;
-        left: 10%;
 
-        // animacao
-        animation: rotation 2s infinite;
-        
-        & .polygon {
+      & .loading-style {
+        // medidas
+        width: 100%;
+        height: 100%;
+
+        & .ring {
           // medidas
-          height: 100%;
-          aspect-ratio: 1;
-  
-          // estilo;
-          background-color: v-bind('style.value.especialColor');
-          clip-path: polygon(50% 0%, 93% 75%, 7% 75%);
+          height: 60%;
+          width: 60%;
+    
+          // posicionamento
+          position: absolute;
+          top: 20%;
+          left: 20%;
+    
+          // estilo
+          border-radius: 50%;
+          background-color: v-bind('style.value.baseColor');
+          border: v-bind('style.value.especialColor') solid 4px;
+          box-sizing: border-box;
         }
-      }
+
+        & .polygon-container {
+          // medidas
+          height: 80%;
+          width: 80%;
+    
+          // estilo
+          border-radius: 50%;
+    
+          // posicionamento
+          position: absolute;
+          top: 10%;
+          left: 10%;
+          
+          & .polygon {
+            // medidas
+            height: 100%;
+            aspect-ratio: 1;
+            
+            // estilo;
+            background-color: v-bind('style.value.especialColor');
+            clip-path: polygon(50% 0%, 93% 75%, 7% 75%);
+          }
+        }
+
+        animation: rotation 2.5s infinite;
+      }        
     }
   }
-
 
   @keyframes rotation {
     25% {
