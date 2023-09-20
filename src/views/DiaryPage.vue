1921fb6039343982b71fa3aec6840c9851a80ef5
vinicius silva <vinicius_dev@hotmail.com>
2023-09-20 20:00:58 -0300

    refactor(DiaryPage.vue): Implementar DayCard ao component
    - Importar o tipo do card -> indica o conteudo a ser renderizado
    
    - DayCard
      - content -> renderizar
      - date -> faz um parce para Date [recebe number -> retorna date]
    	modified:   src/views/DiaryPage.vue

--- src/views/DiaryPage.vue
+++ src/views/DiaryPage.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
+import DayCard from '@/components/dayCard/DayCard.vue'
+import { ref } from 'vue';
+import { type ICard } from '@/stores/DiaryApi'
+
+const cards = ref<ICard[]>()
 </script>
 
 <template>
   <div class="container">
+    <DayCard 
+      v-for="({content, date}, i) in cards"
+      :key="i"
+      :content="content"
+      :date="new Date(date)"
+    />
   </div>
 </template>
 
