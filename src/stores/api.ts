c3272fd213314762ba2c4c4bfb3a3327614962ff
vinicius silva <vinicius_dev@hotmail.com>
2023-09-21 19:25:51 -0300

    feat(stores/api): Criar conexao coma api atraves da url
    - usar url base para conexao com a api
    
    - adicionar a props
      - apiUrl
        - user -> que contera todas os metodos de requiscoes para lidar com o usuario
    	modified:   src/stores/api.ts

--- src/stores/api.ts
+++ src/stores/api.ts
@@ -4,8 +4,16 @@ import type { IUser } from '@/interfaces'
 
 export const useApi = defineStore('api', () => {
 
+  const conectApiUrl = 'https://diario-api-production.up.railway.app'
+
   const props = {
     loading: (state: boolean) => useToggleComponents().toggleGlobalLoading(state),
+    apiUrl: {
+      user: {
+        create: () => `${conectApiUrl}/user/create`,
+        login: () => `${conectApiUrl}/user/login`,
+      },
+    }
   }
 
   const user = () => {
