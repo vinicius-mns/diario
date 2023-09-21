97849d004c5430359ac9eed0680610f03177d2f7
vinicius silva <vinicius_dev@hotmail.com>
2023-09-21 19:21:49 -0300

    feat(stores/api): Definir objeto user que retona create e LoginPage
    	modified:   src/stores/api.ts

--- src/stores/api.ts
+++ src/stores/api.ts
@@ -1,9 +1,24 @@
 import { defineStore } from "pinia"
 import { useToggleComponents } from "./toggleComponents"
+import type { IUser } from '@/interfaces'
 
 export const useApi = defineStore('api', () => {
 
   const props = {
     loading: (state: boolean) => useToggleComponents().toggleGlobalLoading(state),
+  }
+
+  const user = () => {
+
+    const create = (data: IUser) => {
+    }
+
+    const login = (date: Iuser) => {
+    }
+
+    return {
+      create,
+      login,
+    }
   }
 })
