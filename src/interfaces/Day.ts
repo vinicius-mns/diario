003aacfe701b63004ecc471e6868a8f7c1101f53
vinicius silva <vinicius_dev@hotmail.com>
2023-09-21 22:03:30 -0300

    refactor(interfaces.ts): Tornar um arquivo unico com todas as interfaces 	deleted:    src/interfaces/Day.ts 	modified:   src/interfaces/index.ts

--- src/interfaces/Day.ts
+++
@@ -1,4 +0,0 @@
-export interface IDay {
-  date: Date,
-  content: string,
-}
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,10 @@
-export * from './Day'
+export interface ICard {
+  date: number;
+  content: string;
+}
+
+export interface IUser {
+  email: string,
+  password: string,
+  remember: boolean
+}
