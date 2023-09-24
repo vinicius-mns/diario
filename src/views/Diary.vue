b0baeef273a45db42a29200c89493a6bc505ae58
vinicius silva <vinicius_dev@hotmail.com>
2023-09-24 01:27:32 -0300

    feat(): Criar component Diary
    Responsavel por conter rotas:
     - CARDS > renderizar os cards do Dia
     - CARD_ID > renderizar um card unico
    	new file:   src/views/Diary.vue

---
+++ src/views/Diary.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  import TopBar from '@/components/topBar/TopBar.vue';
+</script>
+
+<template>
+  <div>
+    <TopBar />
+    <div class="routes">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+  .routes {
+    height: 100vh;
+    overflow-y: scroll;
+  }
+}
+</style>
