649f4366b0567e84e06d96ffe077f9fbef970602
vinicius silva <vinicius_dev@hotmail.com>
2023-09-24 01:31:28 -0300

    refactor(DiaryPage.vue): Renomar para de Diary para DiaryPage.vue 	modified:   src/views/DiaryPage.vue

--- src/views/DiaryPage.vue
+++ src/views/DiaryPage.vue
@@ -1,24 +1,21 @@
 <script setup lang="ts">
-import DayCard from '@/components/dayCard/DayCard.vue'
-import { ref } from 'vue';
-import { type ICard } from '@/stores/DiaryApi'
-
-const cards = ref<ICard[]>()
+  import TopBar from '@/components/topBar/TopBar.vue';
 </script>
 
 <template>
-  <div class="container">
-    <DayCard 
-      v-for="({content, date}, i) in cards"
-      :key="i"
-      :content="content"
-      :date="new Date(date)"
-    />
+  <div>
+    <TopBar />
+    <div class="routes">
+      <RouterView />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 @media screen and (max-width: 700px) {
-
+  .routes {
+    height: 100vh;
+    overflow-y: scroll;
+  }
 }
 </style>
