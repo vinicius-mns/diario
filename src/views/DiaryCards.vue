888ff4f28f6295ffcc4e1b448ad1b716e813132d
vinicius silva <vinicius_dev@hotmail.com>
2023-09-25 01:41:21 -0300

    feat(): Adicionar pagina de renderizacao de cards
    - Pagina Renderiza cards logal ou remoto
    	new file:   src/views/DiaryCards.vue

---
+++ src/views/DiaryCards.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import DayCard from '@/components/dayCard/DayCard.vue'
+import { onMounted, ref } from 'vue';
+import { type ICard } from '@/interfaces'
+import { useCards } from '@/stores/cards'
+import { useApi } from '@/stores/api'
+
+export interface IDiaryProps {
+  get: 'remote' | 'local'
+}
+
+const props = defineProps<IDiaryProps>()
+const cards = ref<ICard[]>()
+const cardsLocalStorage = useCards()
+
+const { diary } = useApi()
+
+const init = async () => {
+  switch(props.get) {
+    case 'local':
+      cards.value = cardsLocalStorage.getCards()
+
+      break
+    case 'remote':
+      cards.value = await diary.card.read() as ICard[]
+
+      break
+    default: 
+      return []
+  }
+}
+
+onMounted(init)
+</script>
+
+<template>
+  <div class="container">
+    <DayCard 
+      v-for="({content, date}, i) in cards"
+      :key="i"
+      :content="content"
+      :date="date"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+
+}
+</style>
