745be90da6f5137f56de800656bd211a36e545bc
vinicius silva <vinicius_dev@hotmail.com>
2023-09-25 02:04:27 -0300

    feat(components/AddOrEditCard.vue): Criar component
    - botao responsavel por criar ou editar um card
    	new file:   src/components/AddOrEditCard/AddOrEditCard.vue

---
+++ src/components/AddOrEditCard/AddOrEditCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button>
+    {{  }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+@media screen and (max-width: 700px) {
+
+}
+</style>
