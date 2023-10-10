3bf982fe8e1f8c36327ed11e7f32772d564505ad
vinicius silva <vinicius_dev@hotmail.com>
2023-10-10 19:27:01 -0300

    feat(stores): Criar index de stores
    - store global em um unico aquivo
    	modified:   src/stores/index.ts

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,15 +1,31 @@
+import { reactive } from "vue"
 import { defineStore } from "pinia"
-import { useStyle } from "./local"
+import { useLocalCards, useStyle } from "./local"
+import { useRemoteCards, useRemoteUser } from "./remote"
+import { useTopbarWidget } from "./widgets"
 
 export const useStore = defineStore('useStore', () => {
+
+  const params: { value: 'remote' | 'local' } = reactive({
+    value: 'local'
+  })
 
   const use = {
     local: {
       style: useStyle,
+      cards: useLocalCards,
+    },
+    remote: {
+      cards: useRemoteCards,
+      user: useRemoteUser,
+    },
+    widget: {
+      topbar: useTopbarWidget,
     }
   }
 
   return {
-    use
+    use,
+    params
   }
 })
