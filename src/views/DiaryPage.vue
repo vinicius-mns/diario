aeb93334c2bb42f486309cd3a02f4997085c5491
vinicius silva <vinicius_dev@hotmail.com>
2023-10-28 16:53:00 -0300

    feat(DiaryPage): Definir pagina 	modified:   src/views/DiaryPage.vue

--- src/views/DiaryPage.vue
+++ src/views/DiaryPage.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-  import TopBar from '@/components/topBar/TopBar.vue';
+  import TopBar from '@/components/widgets/topBar/TopBar.vue';
 </script>
 
 <template>
   <div>
-    <TopBar />
     <div class="routes">
       <RouterView />
     </div>
+    <TopBar />
   </div>
 </template>
 
 <style scoped lang="scss">
 @media screen and (max-width: 700px) {
   .routes {
-    height: 100vh;
-    overflow-y: scroll;
+    padding-top: 57px;
+    padding-bottom: 20px;
   }
 }
 </style>
