726b9edcbd042d831d500091ac45e578c5fbecd4
vinicius silva <vinicius_dev@hotmail.com>
2023-10-28 16:52:30 -0300

    feat(DiaryCards): Definir component 	modified:   src/views/DiaryCards.vue

--- src/views/DiaryCards.vue
+++ src/views/DiaryCards.vue
@@ -1,66 +1,45 @@
 <script setup lang="ts">
 import DayCard from '@/components/dayCard/DayCard.vue'
-import { onMounted, ref } from 'vue';
-import { type ICard } from '@/interfaces'
-import { useCards } from '@/stores/cards'
-import { useApi } from '@/stores/api'
 import AddOrEditCard from '@/components/AddOrEditCard/AddOrEditCard.vue'
+import { useStore } from '@/stores'
+import { onMounted } from 'vue';
 
 export interface IDiaryProps {
   get: 'remote' | 'local'
 }
 
 const props = defineProps<IDiaryProps>()
-const cards = ref<ICard[]>()
-const cardsLocalStorage = useCards()
-const lastElement = ref<HTMLElement>()
 
-const { diary } = useApi()
+const store = useStore()
 
-const init = async () => {
-  switch(props.get) {
-    case 'local':
-      cards.value = cardsLocalStorage.getCards()
+const { state, init } = store.use[props.get].cards()
 
-      break
-    case 'remote':
-      cards.value = await diary.card.read() as ICard[]
-
-      break
-    default: 
-      return []
-  }
-  
-  const autoScroll = () => lastElement.value?.scrollIntoView({behavior: 'smooth'})
-  setTimeout(autoScroll, 0)
-}
-
-onMounted(init)
+onMounted(async () => {
+  await init()
+})
 </script>
 
 <template>
   <div class="container">
     <DayCard 
-      v-for="({content, date}, i) in cards"
+      v-for="({content, date}, i) in state.cards.value"
       :key="i"
       :content="content"
       :date="date"
     />
-    <AddOrEditCard />
-    <a ref="lastElement"></a>
+    <AddOrEditCard
+      :get="props.get"
+      :typeText="state.typeEditor.value"
+    />
   </div>
 </template>
 
 <style scoped lang="scss">
 @media screen and (max-width: 700px) {
   .container {
-    // padding
-    padding-bottom: 100px;
-    padding-top: 60px;
-
     // display
     display: flex;
-    flex-direction: column;
+    flex-direction: column-reverse;
     align-items: center;
     
     // cores
